50571470197b8daaeb2912e978e7219430b9c2a8
John Cortes <john@Johns-MacBook-Air.local>
2026-08-25 09:13:29 -0500

    use Effect implementation

---
+++ src/App/AppUI.js
@@ -0,0 +1,52 @@
+import { TodoCounter } from '../TodoCounter';
+import { TodoSearch } from '../TodoSearch';
+import { TodoList } from '../TodoList';
+import { TodoItem } from '../TodoItem';
+import { CreateTodoButton } from '../CreateTodoButton';
+
+function AppUI({
+  loading,
+  error,
+  completedTodos,
+  totalTodos,
+  searchValue,
+  setSearchValue,
+  searchedTodos,
+  completeTodo,
+  deleteTodo,
+}) {
+  return (
+    <>
+      <TodoCounter
+        completed={completedTodos}
+        total={totalTodos} 
+      />
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
+
+      <TodoList>
+        {loading && (
+          <p>Cargando ...</p>
+        )}
+        {error && <p>Error!!!</p>}
+        {(!loading && searchedTodos.length === 0) && <p>No hay TODOS disponibles</p>}
+
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+      
+      <CreateTodoButton />
+    </>
+  );
+}
+
+export { AppUI };
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -2,8 +2,7 @@ import './todoSearch.css';
 import React from 'react';
 
 function TodoSearch({searchValue, setSearchValue}) {
-    
-    console.log("Los usuarios buscan TODOs de: " +searchValue);
+
     return (
         <input className="TodoSearch" 
         onChange={(event) => {
